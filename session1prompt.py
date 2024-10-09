a4bffe1c1cb4bfceaf11e2081d1b3db541f584cb
JaimeMaillard <jmaillar@ucsc.edu>
2024-10-09 10:42:46 -0700

    Add files via upload

---
+++ session1prompt.py
@@ -0,0 +1,9 @@
+#this program will write my name and two sentences about me
+
+def aboutme():
+    print("My name is Jaime Daniel Maillard Lopez, my favorite food is menudo and my favorite movie is Inglorious Basterds directed by Tarantino")
+def main():
+    aboutme()
+    
+if __name__=="__main__":
+    main()
