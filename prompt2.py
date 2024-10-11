a5c3d06217cf6949dcd9b097032aad85b026827a
JaimeMaillard <jmaillar@ucsc.edu>
2024-10-10 19:16:59 -0700

    Add files via upload

---
+++ prompt2.py
@@ -0,0 +1,17 @@
+#this program will print the sum of two floating point number, the difference between two integers, and the product of a flaoting point and an integer. each case the program will display the type of answer, float or integer
+
+import numpy as np
+
+n=1.3
+x=5.7
+y=9
+z=12
+
+print("n+x=",n+x)
+print(type(n+x))
+
+print("z-y=",z-y)
+print(type(z-y))
+
+print("n*z=",n*z)
+print(type(n*z))
